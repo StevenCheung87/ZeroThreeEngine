2ea38d0665f717b4788e57b3795ca58a8002191e
StevenCheng87 <stevencheung0690@outlook.com>
2021-08-05 13:27:43 +0100

    Changed file name from "maths" to "mathematics"

---
+++ core/mathematics/matrix.cpp
@@ -0,0 +1,442 @@
+#include "matrix.h"
+
+
+#pragma mark-constructors
+Matrix3::Matrix3()
+{
+    // 3x3 matrix - column major. X vector is 0, 1, 2, etc. (openGL prefer way)
+    //    0    3    6
+    //    1    4    7
+    //    2    5    8
+
+    for (int i=0; i<9;i++)
+    {
+        MatrixData[i]=0.0f;
+    }
+
+    MatrixData[0] = MatrixData[4] = MatrixData[8] = 1.0f;
+}
+
+Matrix3::Matrix3(float m0, float m3, float m6, float m1, float m4, float m7, float m2, float m5, float m8)
+{
+    // 3x3 matrix - column major. X vector is 0, 1, 2, etc. (openGL prefer way)
+    //    0    3    6
+    //    1    4    7
+    //    2    5    8
+
+    MatrixData[0] = m0;
+    MatrixData[3] = m3;
+    MatrixData[6] = m6;
+
+    MatrixData[1] = m1;
+    MatrixData[4] = m4;
+    MatrixData[7] = m7;
+
+    MatrixData[2] = m2;
+    MatrixData[5] = m5;
+    MatrixData[8] = m8;
+}
+
+
+#pragma mark-destructors
+Matrix3::~Matrix3(){}
+
+
+#pragma mark-copy constructors
+Matrix3& Matrix3::operator=(const Matrix3 &value)
+{
+    for (int i = 0; i < 9; i++)
+    {
+        MatrixData[i] = value.MatrixData[i];
+    }
+
+    return *this;
+}
+
+
+#pragma mark-debug
+void Matrix3::Show()
+{
+    // 3x3 matrix - column major. X vector is 0, 1, 2, etc. (openGL prefer way)
+    //    0    3    6
+    //    1    4    7
+    //    2    5    8
+
+    std::cout << "[" << MatrixData[0] << "," << MatrixData[3] << "," << MatrixData[6] << std::endl
+                << MatrixData[1] << "," << MatrixData[4] << "," << MatrixData[7] << std::endl
+                << MatrixData[2] << "," << MatrixData[5] << "," << MatrixData[8] << "]" << std::endl;
+}
+
+
+#pragma mark-matrix addition
+Matrix3 Matrix3::operator+(const Matrix3 &m)
+{
+    Matrix3 n;
+
+    // 3x3 matrix - column major. X vector is 0, 1, 2, etc. (openGL prefer way)
+    //    0    3    6
+    //    1    4    7
+    //    2    5    8
+
+    n.MatrixData[0] = MatrixData[0] + m.MatrixData[0];
+    n.MatrixData[3] = MatrixData[3] + m.MatrixData[3];
+    n.MatrixData[6] = MatrixData[6] + m.MatrixData[6];
+
+    n.MatrixData[1] = MatrixData[1] + m.MatrixData[1];
+    n.MatrixData[4] = MatrixData[4] + m.MatrixData[4];
+    n.MatrixData[7] = MatrixData[7] + m.MatrixData[7];
+
+    n.MatrixData[2] = MatrixData[2] + m.MatrixData[2];
+    n.MatrixData[5] = MatrixData[5] + m.MatrixData[5];
+    n.MatrixData[8] = MatrixData[8] + m.MatrixData[8];
+
+    return n;
+}
+
+void Matrix3::operator+=(const Matrix3 &m)
+{
+    // 3x3 matrix - column major. X vector is 0, 1, 2, etc. (openGL prefer way)
+    //    0    3    6
+    //    1    4    7
+    //    2    5    8
+
+    MatrixData[0] += m.MatrixData[0];
+    MatrixData[3] += m.MatrixData[3];
+    MatrixData[6] += m.MatrixData[6];
+
+    MatrixData[1] += m.MatrixData[1];
+    MatrixData[4] += m.MatrixData[4];
+    MatrixData[7] += m.MatrixData[7];
+
+    MatrixData[2] += m.MatrixData[2];
+    MatrixData[5] += m.MatrixData[5];
+    MatrixData[8] += m.MatrixData[8];
+}
+
+
+#pragma mark-matrix subtraction
+Matrix3 Matrix3::operator-(const Matrix3 &m)
+{
+    Matrix3 n;
+
+    // 3x3 matrix - column major. X vector is 0, 1, 2, etc. (openGL prefer way)
+    //    0    3    6
+    //    1    4    7
+    //    2    5    8
+
+    n.MatrixData[0] = MatrixData[0] - m.MatrixData[0];
+    n.MatrixData[3] = MatrixData[3] - m.MatrixData[3];
+    n.MatrixData[6] = MatrixData[6] - m.MatrixData[6];
+
+    n.MatrixData[1] = MatrixData[1] - m.MatrixData[1];
+    n.MatrixData[4] = MatrixData[4] - m.MatrixData[4];
+    n.MatrixData[7] = MatrixData[7] - m.MatrixData[7];
+
+    n.MatrixData[2] = MatrixData[2] - m.MatrixData[2];
+    n.MatrixData[5] = MatrixData[5] - m.MatrixData[5];
+    n.MatrixData[8] = MatrixData[8] - m.MatrixData[8];
+
+    return n;
+}
+
+void Matrix3::operator-=(const Matrix3 &m)
+{
+    // 3x3 matrix - column major. X vector is 0, 1, 2, etc. (openGL prefer way)
+    //    0    3    6
+    //    1    4    7
+    //    2    5    8
+
+    MatrixData[0] -= m.MatrixData[0];
+    MatrixData[3] -= m.MatrixData[3];
+    MatrixData[6] -= m.MatrixData[6];
+
+    MatrixData[1] -= m.MatrixData[1];
+    MatrixData[4] -= m.MatrixData[4];
+    MatrixData[7] -= m.MatrixData[7];
+
+    MatrixData[2] -= m.MatrixData[2];
+    MatrixData[5] -= m.MatrixData[5];
+    MatrixData[8] -= m.MatrixData[8];
+}
+
+#pragma mark-matrix multiplication scalar
+Matrix3 Matrix3::operator*(const float s)
+{
+    Matrix3 n;
+
+    // 3x3 matrix - column major. X vector is 0, 1, 2, etc. (openGL prefer way)
+    //    0    3    6
+    //    1    4    7
+    //    2    5    8
+
+    n.MatrixData[0] = MatrixData[0] * s;
+    n.MatrixData[3] = MatrixData[3] * s;
+    n.MatrixData[6] = MatrixData[6] * s;
+
+    n.MatrixData[1] = MatrixData[1] * s;
+    n.MatrixData[4] = MatrixData[4] * s;
+    n.MatrixData[7] = MatrixData[7] * s;
+
+    n.MatrixData[2] = MatrixData[2] * s;
+    n.MatrixData[5] = MatrixData[5] * s;
+    n.MatrixData[8] = MatrixData[8] * s;
+
+    return n;
+}
+
+void Matrix3::operator*=(const float s)
+{
+    // 3x3 matrix - column major. X vector is 0, 1, 2, etc. (openGL prefer way)
+    //    0    3    6
+    //    1    4    7
+    //    2    5    8
+
+    MatrixData[3] *= s;
+    MatrixData[0] *= s;
+    MatrixData[6] *= s;
+
+    MatrixData[1] *= s;
+    MatrixData[4] *= s;
+    MatrixData[7] *= s;
+
+    MatrixData[2] *= s;
+    MatrixData[5] *= s;
+    MatrixData[8] *= s;
+}
+
+#pragma mark-matrix division scalar
+Matrix3 Matrix3::operator/(const float s)
+{
+    Matrix3 n;
+
+    // 3x3 matrix - column major. X vector is 0, 1, 2, etc. (openGL prefer way)
+    //    0    3    6
+    //    1    4    7
+    //    2    5    8
+
+    n.MatrixData[0] = MatrixData[0] / s;
+    n.MatrixData[3] = MatrixData[3] / s;
+    n.MatrixData[6] = MatrixData[6] / s;
+
+    n.MatrixData[1] = MatrixData[1] / s;
+    n.MatrixData[4] = MatrixData[4] / s;
+    n.MatrixData[7] = MatrixData[7] / s;
+
+    n.MatrixData[2] = MatrixData[2] / s;
+    n.MatrixData[5] = MatrixData[5] / s;
+    n.MatrixData[8] = MatrixData[8] / s;
+
+    return n;
+}
+
+void Matrix3::operator/=(const float s)
+{
+    // 3x3 matrix - column major. X vector is 0, 1, 2, etc. (openGL prefer way)
+    //    0    3    6
+    //    1    4    7
+    //    2    5    8
+
+    MatrixData[0] /= s;
+    MatrixData[3] /= s;
+    MatrixData[6] /= s;
+
+    MatrixData[1] /= s;
+    MatrixData[4] /= s;
+    MatrixData[7] /= s;
+
+    MatrixData[2] /= s;
+    MatrixData[5] /= s;
+    MatrixData[8] /= s;
+}
+
+
+#pragma mark-vector transfomation
+Vector3 Matrix3::operator*(const Vector3 &v)
+    const
+{
+    // 3x3 matrix - column major. X vector is 0, 1, 2, etc. (openGL prefer way)
+    //    0    3    6
+    //    1    4    7
+    //    2    5    8
+
+    return Vector3(MatrixData[0] * v.x + MatrixData[3] * v.y + MatrixData[6] * v.z,
+                   MatrixData[1] * v.x + MatrixData[4] * v.y + MatrixData[7] * v.z,
+                   MatrixData[2] * v.x + MatrixData[5] * v.y + MatrixData[8] * v.z);
+}
+
+Vector3 Matrix3::TransformVectorByMatrix(const Vector3 &v)
+    const
+{
+    return (*this) * v;
+}
+
+
+#pragma mark-matrix multiplication
+Matrix3 Matrix3::operator*(const Matrix3 &m)
+{
+    // 3x3 matrix - column major. X vector is 0, 1, 2, etc. (openGL prefer way)
+    //    0    3    6
+    //    1    4    7
+    //    2    5    8
+
+    return Matrix3(MatrixData[0] * m.MatrixData[0] + MatrixData[3] * m.MatrixData[1] + MatrixData[6] * m.MatrixData[2],
+                   MatrixData[0] * m.MatrixData[3] + MatrixData[3] * m.MatrixData[4] + MatrixData[6] * m.MatrixData[5],
+                   MatrixData[0] * m.MatrixData[6] + MatrixData[3] * m.MatrixData[7] + MatrixData[6] * m.MatrixData[8],
+
+                   MatrixData[1] * m.MatrixData[0] + MatrixData[4] * m.MatrixData[1] + MatrixData[7] * m.MatrixData[2],
+                   MatrixData[1] * m.MatrixData[3] + MatrixData[4] * m.MatrixData[4] + MatrixData[7] * m.MatrixData[5],
+                   MatrixData[1] * m.MatrixData[6] + MatrixData[4] * m.MatrixData[7] + MatrixData[7] * m.MatrixData[8],
+
+                   MatrixData[2] * m.MatrixData[0] + MatrixData[5] * m.MatrixData[1] + MatrixData[8] * m.MatrixData[2],
+                   MatrixData[2] * m.MatrixData[3] + MatrixData[5] * m.MatrixData[4] + MatrixData[8] * m.MatrixData[5],
+                   MatrixData[2] * m.MatrixData[6] + MatrixData[5] * m.MatrixData[7] + MatrixData[8] * m.MatrixData[8]);
+}
+
+void Matrix3::operator*=(const Matrix3 &m)
+{
+    // 3x3 matrix - column major. X vector is 0, 1, 2, etc. (openGL prefer way)
+    //    0    3    6
+    //    1    4    7
+    //    2    5    8
+
+    float t1;
+    float t2;
+    float t3;
+
+    t1 = MatrixData[0] * m.MatrixData[0] + MatrixData[3] * m.MatrixData[1] + MatrixData[6] * m.MatrixData[2];
+    t2 = MatrixData[0] * m.MatrixData[3] + MatrixData[3] * m.MatrixData[4] + MatrixData[6] * m.MatrixData[5];
+    t3 = MatrixData[0] * m.MatrixData[6] + MatrixData[3] * m.MatrixData[7] + MatrixData[6] * m.MatrixData[8];
+
+    MatrixData[0] = t1;
+    MatrixData[3] = t2;
+    MatrixData[6] = t3;
+
+    t1 = MatrixData[1] * m.MatrixData[0] + MatrixData[4] * m.MatrixData[1] + MatrixData[7] * m.MatrixData[2];
+    t2 = MatrixData[1] * m.MatrixData[3] + MatrixData[4] * m.MatrixData[4] + MatrixData[7] * m.MatrixData[5];
+    t3 = MatrixData[1] * m.MatrixData[6] + MatrixData[4] * m.MatrixData[7] + MatrixData[7] * m.MatrixData[8];
+
+    MatrixData[1] = t1;
+    MatrixData[4] = t2;
+    MatrixData[7] = t3;
+
+    t1 = MatrixData[2] * m.MatrixData[0] + MatrixData[5] * m.MatrixData[1] + MatrixData[8] * m.MatrixData[2];
+    t2 = MatrixData[2] * m.MatrixData[3] + MatrixData[5] * m.MatrixData[4] + MatrixData[8] * m.MatrixData[5];
+    t3 = MatrixData[2] * m.MatrixData[6] + MatrixData[5] * m.MatrixData[7] + MatrixData[8] * m.MatrixData[8];
+
+    MatrixData[2] = t1;
+    MatrixData[5] = t2;
+    MatrixData[8] = t3;
+}
+
+
+#pragma mark-matrix identity
+void Matrix3::SetMatrixAsIdentityMatrix()
+{
+    for (int i = 0; i < 9; i++)
+    {
+        MatrixData[i] = 0.0f;
+    }
+
+    MatrixData[0] = MatrixData[4] = MatrixData[8] = 1.0f;
+}
+
+
+#pragma mark-matrix inverse
+void Matrix3::SetMatrixAsInverseOfGivenMatrix(const Matrix3 &m)
+{
+    // 3x3 matrix - column major. X vector is 0, 1, 2, etc. (openGL prefer way)
+    //    0    3    6
+    //    1    4    7
+    //    2    5    8
+
+    float t1 = m.MatrixData[0] * m.MatrixData[4];
+    float t2 = m.MatrixData[0] * m.MatrixData[7];
+    float t3 = m.MatrixData[3] * m.MatrixData[1];
+    float t4 = m.MatrixData[3] * m.MatrixData[1];
+    float t5 = m.MatrixData[6] * m.MatrixData[2];
+    float t6 = m.MatrixData[6] * m.MatrixData[2];
+
+    float determinant = (t1 * m.MatrixData[8] - t2 * m.MatrixData[5] - t3 * m.MatrixData[8] - t4 * m.MatrixData[2] + t5 * m.MatrixData[5] - t6 * m.MatrixData[2]);
+
+    if (determinant == 0)
+    {
+        return;
+    }
+
+    float invd = 1.0f / determinant;
+
+    float m0 = (m.MatrixData[4] * m.MatrixData[8] - m.MatrixData[7] * m.MatrixData[5]) * invd;
+    float m3 = -(m.MatrixData[3] * m.MatrixData[8] - m.MatrixData[6] * m.MatrixData[5]) * invd;
+
+    float m6 = (m.MatrixData[3] * m.MatrixData[7] - m.MatrixData[6] * m.MatrixData[4]) * invd;
+
+    float m1 = -(m.MatrixData[1] * m.MatrixData[8] - m.MatrixData[7] * m.MatrixData[2]) * invd;
+
+    float m4 = (m.MatrixData[0] * m.MatrixData[8] - t6) * invd;
+
+    float m7 = -(t2 - t4) * invd;
+
+    float m2 = (m.MatrixData[1] * m.MatrixData[5] - m.MatrixData[4] * m.MatrixData[2]) * invd;
+
+    float m5 = -(m.MatrixData[0] * m.MatrixData[5] - t5) * invd;
+
+    float m8 = (t1 - t3) * invd;
+
+    MatrixData[0] = m0;
+    MatrixData[3] = m3;
+    MatrixData[6] = m6;
+
+    MatrixData[1] = m1;
+    MatrixData[4] = m4;
+    MatrixData[7] = m7;
+
+    MatrixData[2] = m2;
+    MatrixData[5] = m5;
+    MatrixData[8] = m8;
+}
+
+Matrix3 Matrix3::GetInverseOfMatrix()
+    const
+{
+    Matrix3 result;
+    result.SetMatrixAsInverseOfGivenMatrix(*this);
+    return result;
+}
+
+void Matrix3::InverseMatrix()
+{
+    SetMatrixAsInverseOfGivenMatrix(*this);
+}
+
+
+#pragma mark-matrix transpose
+void Matrix3::SetMatrixAsTransposeOfGivenMatrix(const Matrix3 &m)
+{
+    //3x3 Matrix
+    //    0    3    6
+    //    1    4    7
+    //    2    5    8
+
+    //3x3 transposed
+    //  0   1   2
+    //  3   4   5
+    //  6   7   8
+
+    MatrixData[0] = m.MatrixData[0];
+    MatrixData[3] = m.MatrixData[1];
+    MatrixData[6] = m.MatrixData[2];
+    MatrixData[1] = m.MatrixData[3];
+    MatrixData[4] = m.MatrixData[4];
+    MatrixData[7] = m.MatrixData[5];
+    MatrixData[2] = m.MatrixData[6];
+    MatrixData[5] = m.MatrixData[7];
+    MatrixData[8] = m.MatrixData[8];
+}
+
+Matrix3 Matrix3::GetTransposedMatrix()
+    const
+{
+    Matrix3 result;
+    result.SetMatrixAsTransposeOfGivenMatrix(*this);
+    return result;
+}
---
+++ core/mathematics/vector.h
@@ -0,0 +1,496 @@
+/*************************************************************************/
+/*  vector.h                                                            */
+/*************************************************************************/
+/*                       This file is part of:                           */
+/*                           ZeroThree Engine                            */
+/*************************************************************************/
+/* Copyright (c) 2021-present Steven Cheung.                             */
+/*                                                                       */
+/* Permission is hereby granted, free of charge, to any person obtaining */
+/* a copy of this software and associated documentation files (the       */
+/* "Software"), to deal in the Software without restriction, including   */
+/* without limitation the rights to use, copy, modify, merge, publish,   */
+/* distribute, sublicense, and/or sell copies of the Software, and to    */
+/* permit persons to whom the Software is furnished to do so, subject to */
+/* the following conditions:                                             */
+/*                                                                       */
+/* The above copyright notice and this permission notice shall be        */
+/* included in all copies or substantial portions of the Software.       */
+/*                                                                       */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,       */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF    */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.*/
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY  */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,  */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE     */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
+/*************************************************************************/
+
+#pragma once
+
+#include <iostream>
+#include <math.h>
+
+class Vector2
+{
+    public:
+        /*!
+         *  @brief sx vector component
+         */
+        float x;
+        /*!
+         *  @brief y vector component
+         */
+        float y;
+
+#pragma mark-constructors2D
+        /*!
+        * @brief Constructor to create a 3D vector with x, y, and z components
+        * 
+        * @param uX x component
+        * @param uY y component
+        * 
+        * @return Creates a vector with the given x, y and z components
+        */
+        Vector2(float uX, float uY);
+
+        /*!
+        * @brief Constructor to create a 2D vector with zero vector components.
+        */
+        Vector2();
+
+#pragma mark-destructor2D
+        /*!
+        * @brief 2D vector desctructor
+        */
+        ~Vector2();
+
+#pragma mark-copy constructor2D
+        /*!
+        * @brief Copy constuctor for 2D vector
+        * 
+        * @param a 2D vector to copy
+        * 
+        * @return Copies the values of the vector
+        */
+        Vector2(const Vector2 &v);
+
+        /*!
+         *  @brief  Copy constructor for 2D vector
+         *
+         *  @param a 2D vector to copy
+         *
+         *  @return Copies the values of the vector
+         *
+         */
+        Vector2 &operator=(const Vector2 &v);
+
+#pragma mark-add2D
+        /*!
+        * @brief Calculates the addition of two vectors.
+        * 
+        * @param v Vector to add.
+        * 
+        * @return Resultant of two vectors.
+        */
+        void operator+=(const Vector2 &v);
+
+        /*!
+        * @brief Calculates the addition of two vectors.
+        * 
+        * @param v 2D vector to add.
+        * 
+        * @return Returns a third vector representing the addition of two vectors.
+        */
+        Vector2 operator+(const Vector2 &v)const;
+
+#pragma mark-subraction2D
+        /*!
+        * @brief Calculates the subtraction of two vectors.
+        * 
+        * @param v Vector to subtract.
+        * 
+        * @return Resultant of two vectors.
+        */
+        void operator-=(const Vector2 &v);
+
+        /*!
+        * @brief Calculates the subtraction of two vectors.
+        * 
+        * @param v Vector to subtract.
+        * 
+        * @return Returns a third vector representing the subtraction of two vectors.
+        */
+        Vector2 operator-(const Vector2 &v)const;
+
+#pragma mark-multiplication2D
+        /*!
+        * @brief Calculates the multiplication of a vector by scalar.
+        * 
+        * @param v Scalar.
+        * 
+        * @return Product of multiplcation.
+        */
+        void operator*=(const float v);
+
+        /*!
+        * @brief Calculates the multiplication of a vector by scalar.
+        * 
+        * @param v Scalar.
+        * 
+        * @return Vector representing product.
+        */
+        Vector2 operator*(const float v)const;
+
+#pragma mark-division2D
+        /*!
+         *  @brief  Division of vector by scalar.
+         *
+         *  @param v Scalar
+         *
+         *  @return Quotient of division.
+         */
+        void operator/=(const float v);
+
+        /*!
+         *  @brief  Division of vector by scalar.
+         *
+         *  @param v Scalar
+         *
+         *  @return Quotient of division.
+         */
+        Vector2 operator/(const float v)const;
+
+#pragma mark-modulus2D
+        /*!
+         *  @brief  Modulus of vector by scalar.
+         *
+         *  @param v Scalar
+         *
+         *  @return Resultant of the modulo of vector by scalar.
+         */
+        void operator%=(const float v);
+
+        /*!
+         *  @brief  Modulus of vector by scalar.
+         *
+         *  @param v Scalar
+         *
+         *  @return Returns a third vector representing the modulo of vector by scalar.
+         */
+        Vector2 operator%(const float v)const;
+
+#pragma mark-dot product2D
+        /*!
+         *  @brief  Calculate dot product.
+         *
+         *  @param v Vector
+         *
+         *  @return Resultant of Dot product.
+         */
+        float operator*(const Vector2 &v)const;
+
+        /*!
+         *  @brief  Calculate dot product.
+         *
+         *  @param v Vector
+         *
+         *  @return Return the Dot product.
+         */
+        float dot(const Vector2 &v)const;
+
+#pragma mark-length2D
+        /*!
+         *  @brief  Calculate the length of vector.
+         *
+         *  @return Length result.
+         */
+        float Length();
+
+#pragma mark-conjugate2D
+        /*!
+         *  @brief  Conjugate the vector.
+         */
+        void Conjugate();
+
+#pragma mark-normalize2D
+        /*!
+         *  @brief  Normalize the vector.
+         */
+        void Normalize();
+
+#pragma mark-clear2D
+        /*!
+         *  @brief  Convert vector to a zero vector
+         */
+        void Zero();
+
+        /*!
+         *  @brief  Calculate absolute value of vector
+         */
+        void Absolute();
+
+#pragma mark-show2D
+        /*!
+         *  @brief  Print vector components
+         */
+        void Show();
+
+        //Negate all components
+        void negate();
+};
+
+class Vector3
+{
+    public:
+        /*!
+         *  @brief x vector component
+         */
+        float x;
+
+        /*!
+         *  @brief y vector component
+         */
+        float y;
+
+        /*!
+         *  @brief z vector component
+         */
+        float z;
+
+#pragma mark-constructors3D
+        /* 
+        * @brief Constructor to create a 3D vector with x,y, and z components
+        * 
+        * @param uX x component 
+        * @param uY y component
+        *  @param uZ z component
+        * 
+        * @return Creates a vector with the given x,y and z components
+        */
+        Vector3(float uX, float uY, float uZ);
+
+        /* 
+        * @brief Constructor to create a 3D vector with zero vector components.
+         */
+        Vector3();
+
+#pragma mark-destructor3D
+        /*
+        * @brief 3D vector desctructor
+        */
+        ~Vector3();
+
+#pragma mark-copy constructor3D
+        /*!
+        * @brief Copy constuctor for 3D vector
+        * 
+        * @param a 3D vector to copy
+        * 
+        * @return Copies the values of the vector
+        */
+        Vector3(const Vector3 &v);
+
+        /*!
+         *  @brief  Copy constructor for 3D vector
+         *
+         *  @param a 3D vector to copy
+         *
+         *  @return Copies the values of the vector
+         *
+         */
+        Vector3 &operator=(const Vector3 &v);
+
+#pragma mark-add3D
+        /*!
+        * @brief Calculates the addition of two vectors.
+        * 
+        * @param v Vector to add.
+        * 
+        * @return Resultant of two vectors.
+        */
+        void operator+=(const Vector3 &v);
+
+        /*!
+        * @brief Calculates the addition of two vectors.
+        * 
+        * @param v 3D vector to add.
+        * 
+        * @return Returns a third vector representing the addition of two vectors.
+        */
+        Vector3 operator+(const Vector3 &v)const;
+
+#pragma mark-subraction3D
+        /*!
+        * @brief Calculates the subtraction of two vectors.
+        * 
+        * @param v Vector to subtract.
+        * 
+        * @return Resultant of two vectors.
+        */
+        void operator-=(const Vector3 &v);
+
+        /*!
+        * @brief Calculates the subtraction of two vectors.
+        * 
+        * @param v Vector to subtract.
+        * 
+        * @return Returns a third vector representing the subtraction of two vectors.
+        */
+        Vector3 operator-(const Vector3 &v)const;
+
+#pragma mark-multiply3D
+        /*!
+        * @brief Calculates the multiplication of a vector by scalar.
+        * 
+        * @param v Scalar.
+        * 
+        * @return Product of multiplcation.
+        */
+        void operator*=(const float v);
+
+        /*!
+        * @brief Calculates the multiplication of a vector by scalar.
+        * 
+        * @param v Scalar.
+        * 
+        * @return Vector representing product.
+        */
+        Vector3 operator*(const float v)const;
+
+#pragma mark-division3D
+        /*!
+         *  @brief  Division of vector by scalar.
+         *
+         *  @param v Scalar
+         *
+         *  @return Quotient of division.
+         */
+        void operator/=(const float v);
+
+        /*!
+         *  @brief  Division of vector by scalar.
+         *
+         *  @param v Scalar
+         *
+         *  @return Quotient of division.
+         */
+        Vector3 operator/(const float v)const;
+
+#pragma mark-modulus3D
+        /*!
+         *  @brief  Modulus of vector by scalar.
+         *
+         *  @param v Scalar
+         *
+         *  @return Resultant of the modulo of vector by scalar.
+         */
+        void operator%=(const float v);
+
+        /*!
+         *  @brief  Modulus of vector by scalar.
+         *
+         *  @param v Scalar
+         *
+         *  @return Returns a third vector representing the modulo of vector by scalar.
+         */
+        Vector3 operator%(const float v);
+
+#pragma mark-dot product3D
+        /*!
+         *  @brief  Calculate dot product.
+         *
+         *  @param v Vector
+         *
+         *  @return Dot product result.
+         */
+        float operator*(const Vector3 &v)const;
+
+        /*!
+         *  @brief  Calculate dot product.
+         *
+         *  @param v Vector
+         *
+         *  @return Dot product result.
+         */
+        float dot(const Vector3 &v)const;
+
+        /*!
+         *  @brief  Calculates the angle between vectors.
+         *
+         *  @param v Vector
+         *
+         *  @return Returns the angle between vectors in degrees.
+         */
+        float angle(const Vector3 &v);
+
+        /*!
+         *  @brief  Calculate cross product.
+         *
+         *  @param v Vector
+         *
+         *  @return Cross product result.
+         */
+        void operator%=(const Vector3 &v);
+
+        /*!
+         *  @brief  Calculate cross product.
+         *
+         *  @param v Vector
+         *
+         *  @return Cross product result.
+         */
+        Vector3 operator%(const Vector3 &v)const;
+
+        /*!
+         *  @brief  Calculate cross product.
+         *
+         *  @param v Vector
+         *
+         *  @return Cross product result.
+         */
+        Vector3 cross(const Vector3 &v)const;
+
+#pragma mark-conjugate3D
+        /*!
+         *  @brief  Conjugate the vector.
+         */
+        void Conjugate();
+
+#pragma mark-normalize3D
+        /*!
+         *  @brief  Normalize the vector.
+         */
+        void Normalize();
+
+#pragma mark-clear3D
+        /*!
+         *  @brief  Convert vector to a zero vector
+         */
+        void Zero();
+        
+        /*!
+         *  @brief  Calculate absolute value of vector
+         */
+        void Absolute();
+
+#pragma mark-show3D
+        /*!
+         *  @brief  Print vector components
+         */
+        void Show();
+        
+        //Negate all components
+        void negate();
+
+#pragma mark-rotating vector by quaternion
+        /*!
+        *  @brief Rotate a vector about an angle using quaternions
+        *
+        *  @param uAngle angle to rotate
+        *  @param uAxis  axis to rotate about
+        *
+        *  @return Vector rotation
+        */
+        Vector3 RotateVectorAboutAngleAndAxis(float uAngle, Vector3 &uAxis);
+};
